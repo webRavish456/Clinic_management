532c4f4591e3b179d6601cfb51ff42879f019415
SnehanjaliBiswal <biswalsnehanjali77@gmail.com>
2025-04-10 12:08:18 +0530

    commit

--- src/Branch/Delete/Delete.js
+++ src/Branch/Delete/Delete.js
@@ -1,21 +1,23 @@
 import { Box, Button } from "@mui/material";
 import React from "react"
 
-const DeleteStaff = ({handleClose, handleDelete}) =>
+const DeleteBranch= ({handleClose, handleDelete}) =>
 {
      return (
      <>
+    
            <Box sx={{ width: '400px', padding: '20px', bgcolor: 'background.paper',textAlign:'flex-end' }}>
              Are you sure want to delete?
            </Box>
            <Box className="submit" sx={{ display: 'flex', justifyContent: 'flex-end', gap: '15px', margin: '20px'}}>
             <Button onClick={handleClose} className="secondary_button" >Cancel</Button>
-            <Button onClick={handleDelete} className="delete_button">
-              Delete
+            <Button  onClick={handleDelete} className="delete_button">
+            Delete
             </Button>
+
             </Box>
      </>
      )
 }
 
-export default DeleteStaff;
+export default DeleteBranch;
---
+++ src/Layout/LayoutAppointment.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from "../Component/Header/Header";
+import Sidebar from "../Component/Sidebar/Sidebar";
+import Appointment from "../Appointment/Appointment";
+
+
+const LayoutAppointment=()=>
+{
+    return (
+        <>
+          <div className="layout">
+        <div className="main-container">
+            <Sidebar/>
+            <div className="content">
+            <Header className="header" />
+            
+            <Appointment className="dashboard" />
+            </div>
+        </div>
+        </div>
+
+
+        </>
+    )
+}
+
+export default LayoutAppointment;
